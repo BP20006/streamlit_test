331e2e8f6694113ff6806c72270b1850cff13410
BP20006 <83199951+BP20006@users.noreply.github.com>
2025-01-09 21:09:58 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,6 +1,21 @@
 import streamlit as st
+from PIL import Image
+import numpy as np
+import cv2
 
-# st.write()はMarkdown表記対応
-st.write('# headline1')
-# 以下のように明示的に書くことも可能
-st.markdown('# headline2')
+st.title("Image Converter")
+  
+uploaded_file = st.file_uploader('Upload your image file.')
+  
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    image = np.array(image)
+
+    st.subheader('Uploaded image')
+    st.image(image)
+
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    st.subheader('Converted image')
+    st.image(gray_image)
+  
