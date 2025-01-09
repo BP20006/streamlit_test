2f1afef0407fa56144c434d2e3b31b6e4b93cbf4
BP20006 <83199951+BP20006@users.noreply.github.com>
2025-01-09 22:59:49 +0900

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,9 +1,69 @@
 import streamlit as st
 import pandas as pd
+import pyperclip
 
-if __name__ == "__main__":
+def create_ChatGPT_script(title, abstract, url):
+    # フォーマットに埋め込む
+    script = f"""
+まず、下記の文章を日本語で短く要約したものを「日本語概要」とラベル付けしてください。
+{abstract}
+次に、以下のフォーマットに「日本語概要」を加えてそれのみを出力してください。
+タイトル：
+{title}
+概要：
+「日本語概要」
+URL：
+{url}
+"""
 
-    df = pd.read_csv('data/2024-12-11.csv', sep=',')
+    return script
 
-    st.header('arxivチェック支援サイト')
-    st.write(df)
+
+st.set_page_config(
+    page_title="Ex-stream-ly Cool App",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
+
+st.sidebar.markdown("[ChatGPTのリンク](https://chat.openai.com)")
+
+# サイドバーでCSVファイルを選択
+csv_files = {
+    "12/11": "output/2024-12-11.csv",
+    "12/12": "output/2024-12-11.csv",
+    "12/13": "output/2024-12-11.csv",
+    "12/14": "output/2024-12-11.csv",
+    "12/15": "output/2024-12-11.csv"
+}
+
+selected_file = st.sidebar.selectbox("表示する日にちを選んでください", list(csv_files.keys()))
+
+# 選択されたCSVファイルを読み込む
+csv_file = csv_files[selected_file]
+data = pd.read_csv(csv_file)
+
+# タイトルの設定
+st.header("arXivチェック支援サイト")
+st.subheader("共有までの手順")
+st.text('1. 自分が担当する日にちをサイドバーから選ぶ')
+st.text('2. 気になる論文をクリック')
+st.text('3. ChatGPTのスクリプトをコピーする')
+st.text('4. ChatGPTにスクリプトを送信して共有用文章を作成')
+st.text('5. Slackで共有')
+
+# 行ごとに情報を取得
+for index, row in data.iterrows():
+    title_trans = row['title_trans'].replace("タイトル：", "")
+    with st.expander(f"{index + 1} : {title_trans}"):
+        # 最初はtitle_transを表示
+        st.write(f"**Title**: {row['title']}")
+        st.write(f"**Abstract**: {row['abstract']}")
+        st.write(f"**Abstract Translated**: {row['abst_trans']}")
+
+        # ChatGPTスクリプト作成（仮の関数）
+        for_chat_GPT = create_ChatGPT_script(row['title'], row['abstract'], "sample.com")
+
+        if st.button("ChatGPTスクリプト", key=f"button_{index}"):
+            pyperclip.copy(for_chat_GPT)  # 文字列をクリップボードにコピー
+            st.success("文字列がクリップボードにコピーされました！")
+
