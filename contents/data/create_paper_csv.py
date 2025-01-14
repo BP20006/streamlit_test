d5a133ce0dda8a1af2997f583c2dac41a69b7479
BP20006 <83199951+BP20006@users.noreply.github.com>
2025-01-14 10:28:15 +0900

    Create create_paper_csv.py

---
+++ contents/data/create_paper_csv.py
@@ -0,0 +1,60 @@
+from googletrans import Translator
+import csv
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime, timedelta
+
+def find_paper_ifno(paper):
+  # 各論文ページのHTMLから情報を取得
+  paper_url = paper.find("a").get("href")
+  paper_html = requests.get(paper_url).text
+  paper_soup = BeautifulSoup(paper_html, "html.parser")
+
+  # 投稿日時が実行時の日付から２日以内なら論文情報を取得
+  paper_author = paper_soup.find("div", class_="authors").text
+  paper_title = paper_soup.find("h1", class_="title mathjax").text
+  paper_abst = paper_soup.find("blockquote", class_="abstract mathjax").text
+
+  # 日付と論文の情報（投稿日、著者、タイトル、概要）を返す
+  return [paper_author, paper_title.replace('\n', ''), paper_abst.replace('\n', '')]
+
+
+def translate_paper_info(paper_info):
+  translator = Translator()
+  paper_info.append(translator.translate(paper_info[1], src='en', dest='ja').text)
+  paper_info.append(translator.translate(paper_info[2], src='en', dest='ja').text)
+
+  return paper_info
+
+def create_papers_csv(today)
+  # arxivのURL（検索単語は"quantum physics"）
+  # １ページの論文数は50
+  target_date = (today - timedelta(days=3)).strftime('%Y-%m-%d')
+  next_date = (today - timedelta(days=2)).strftime('%Y-%m-%d')
+  pagesize = 200
+  start_num = 0
+  arxiv_url = f'https://arxiv.org/search/advanced?advanced=&terms-0-operator=AND&terms-0-term=quantum+physics&terms-0-field=all&classification-physics_archives=all&classification-include_cross_list=include&date-year=&date-filter_by=date_range&date-from_date={target_date}&date-to_date={next_date}&date-date_type=submitted_date&abstracts=show&size={pagesize}&order=-announced_date_first&start={start_num}'
+  # CSVファイルにデータを書き込む
+  f = open(f'{target_date}.csv', mode='w', newline='', encoding='utf-8')
+  writer = csv.writer(f)
+  header = ['title', 'authors', 'abstract', 'authors_trans', 'sbstract_stans']
+  writer.writerow(header)
+
+  loop_frag = True
+
+  # htmlを取得
+  arxiv_html_page = requests.get(arxiv_url).text
+  arxiv_soup = BeautifulSoup(arxiv_html_page, "html.parser")
+
+  # 各論文のHTMLに分割
+  papers = arxiv_soup.find_all("li", class_="arxiv-result")
+  for paper in papers:
+
+      paper_info = find_paper_ifno(paper)
+      writer.writerow(translate_paper_info(paper_info))
+
+  f.close()
+
+if __name__ == "__main__":
+  today = datetime.today()
+  create_papers_csv(today)
