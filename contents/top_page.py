b5557d5c9faf75ce86cb758caa616506ce94c812
BP20006 <83199951+BP20006@users.noreply.github.com>
2025-01-14 14:14:48 +0900

    Update top_page.py

--- contents/top_page.py
+++ contents/top_page.py
@@ -2,5 +2,5 @@ import streamlit as st
 st.title("木村研の研究員支援サイトトップページ")
 st.write("左側のサイドバーから使いたい支援ツールを選んでね…")
 
-st.subheeader("同期の研究ブログ")
+st.subheader("同期の研究ブログ")
 st.markdown("[Komnena's Homepage](https://komnena-homepage.vercel.app/)")
