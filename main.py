da8ef2d470a91d1e3119197c69b7cb438cc6189f
BP20006 <83199951+BP20006@users.noreply.github.com>
2025-01-21 00:29:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ st.set_page_config(
 
 top_page = st.Page(page="contents/top_page.py", title="Top", icon=":material/home:")
 display_paper = st.Page(page="contents/display_paper.py", title="arXivチェック支援ツール", icon=":material/open_with:")
-display_paper = st.Page(page="contents/display_paper.py", title="arXivチェック支援ツール", icon=":material/open_with:")
 create_address = st.Page(page="contents/create_address.py", title="学版アドレス作成支援ツール", icon=":material/open_with:")
 assist_latex = st.Page(page="contents/assist_latex.py", title="Latex入力支援ツール", icon=":material/open_with:")
 
