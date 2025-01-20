74a9cd92800359e5404d8a028c47b5687f121928
BP20006 <83199951+BP20006@users.noreply.github.com>
2025-01-21 00:26:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ top_page = st.Page(page="contents/top_page.py", title="Top", icon=":material/hom
 display_paper = st.Page(page="contents/display_paper.py", title="arXivチェック支援ツール", icon=":material/open_with:")
 display_paper = st.Page(page="contents/display_paper.py", title="arXivチェック支援ツール", icon=":material/open_with:")
 create_address = st.Page(page="contents/create_address.py", title="学版アドレス作成支援ツール", icon=":material/open_with:")
+assist_latex = st.Page(page="contents/assist_latex.py", title="Latex入力支援ツール", icon=":material/open_with:")
 
-pg = st.navigation([top_page, display_paper, create_address])
+pg = st.navigation([top_page, display_paper, create_address, assist_latex])
 pg.run()
